4c6c25f36176f28919fd7a7842ec792c3e092317
Tharushi Weerasinghe <tharushiu2000@gmail.com>
2023-05-25 17:51:41 +0530

    Calculation

---
+++ simpleCalculator/Calculate.cs
@@ -0,0 +1,35 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalculator
+{
+   public static class Calculate
+    {
+        public static double DoCalculation (double vall, double va12, string operatorMath)
+        {
+            double result = 0;
+
+            switch (operatorMath)
+            {
+                case "/":
+                    result = vall / va12; 
+                    break;
+                case "-":
+                    result = vall / va12;
+                    break;
+                case "+":
+                    result = vall / va12;
+                    break;
+                case "*":
+                    result = vall / va12;
+                    break;
+
+            }
+            return result;
+        }
+
+    }
+}
--- simpleCalculator/MainPage.xaml.cs
+++ simpleCalculator/MainPage.xaml.cs
@@ -1,25 +1,32 @@
-﻿namespace simpleCalculator;
+﻿ namespace simpleCalculator;
 
 public partial class MainPage : ContentPage
 {
 	int currentState = 1;
 	string operatorMath;
-	double firsNum, secondNum;
+	double firstNum, secondNum;
 
 
 	public MainPage()
 	{
 		InitializeComponent();
+		OnClear(this, null);
 	}
 
 	void OnClear(object sender, EventArgs e)
 	{
-
+        firstNum = 0;
+		secondNum = 0;
+		currentState = 1;
+		this.result.Text = "0";
 	}
 
 	void OnSquareRoot(object sender, EventArgs e)
 	{
-
+		if (firstNum == 0)
+			return;
+		firstNum = firstNum + firstNum;
+		this.result.Text = firstNum.ToString();
 	}
 
 	void OnNumberSelection (object sender, EventArgs e)
@@ -39,7 +46,34 @@ public partial class MainPage : ContentPage
 		double number;
 		if(double.TryParse(this.result.Text,out number))
 		{
+			this.result.Text = number.ToString("NO");
+			if (currentState ==1)
+			{
+				firstNum = number;
+			}
+			else
+			{
+				secondNum = number;
+			}
+		}
+	}
 
+	void onOperatorSelection(object sender, EventArgs e)
+	{
+		currentState = -2;
+		Button button = (Button)sender;
+		string btnPressed = button.Text;
+		operatorMath = btnPressed     ;
+	}
+
+	void onCalculate (object sender, EventArgs e)
+	{
+		if(currentState ==2)
+		{
+			var result = Calculate.DoCalculation(firstNum, secondNum, operatorMath);
+			 this.result.Text = result.ToString();
+			firstNum = result;
+			currentState = -1   ;
 		}
 	}
 
